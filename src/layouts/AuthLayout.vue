ecbcd5334260338f7064fab7d4684d4d14503e55
DanielGraham123 <dg.danielboaz@yahoo.com>
2021-12-07 21:59:48 +0100

    made ui changes to the auth ui's

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,76 +1,10 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <!-- <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header> -->
-    <div class="stars">
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-    </div>
-
-    <q-page-container class="">
-      <keep-alive><router-view /></keep-alive>
+    <q-page-container class="q-py-xl">
+      <div class="text-center q-pb-0 q-mb-md">
+        <q-icon name="img:asri-logo-l.png" size="lg" class="logo-l" />
+      </div>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
@@ -147,132 +81,32 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-@mixin sp-layout {
-  @media screen and (max-width: 750px) {
-    @content;
-  }
+.q-page-container {
+  background: -webkit-linear-gradient(bottom, var(--q-primary), var(--q-info));
+  position: relative;
+  z-index: 4;
 }
 
-body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background: radial-gradient(ellipse at bottom, #0d1d31 0%, #0c0d13 100%);
-  overflow: hidden;
+.logo-l {
+  max-width: 100%;
+  background-size: contain;
+  font-size: 3.8rem !important;
+  width: 185px;
 }
-
-@function random_range($min, $max) {
-  $rand: random();
-  $random_range: $min + floor($rand * (($max - $min) + 1));
-  @return $random_range;
-}
-
-.stars {
-  position: fixed;
+.q-page-container::before {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 120%;
-  transform: rotate(-45deg);
+  background-image: url(../assets/img-01.webp);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 }
-
-.star {
-  $star-count: 50;
-  --star-color: var(--q-info);
-  --star-tail-length: 6em;
-  --star-tail-height: 2px;
-  --star-width: calc(var(--star-tail-length) / 6);
-  --fall-duration: 9s;
-  --tail-fade-duration: var(--fall-duration);
-
-  position: absolute;
-  top: var(--top-offset);
-  left: 0;
-  width: var(--star-tail-length);
-  height: var(--star-tail-height);
-  color: var(--star-color);
-  background: linear-gradient(45deg, currentColor, transparent);
-  border-radius: 50%;
-  filter: drop-shadow(0 0 6px currentColor);
-  transform: translate3d(104em, 0, 0);
-  animation: fall var(--fall-duration) var(--fall-delay) linear infinite,
-    tail-fade var(--tail-fade-duration) var(--fall-delay) ease-out infinite;
-
-  @include sp-layout {
-    // For mobile performance, tail-fade animation will be removed QAQ
-    animation: fall var(--fall-duration) var(--fall-delay) linear infinite;
-  }
-
-  @for $i from 1 through $star-count {
-    &:nth-child(#{$i}) {
-      --star-tail-length: #{random_range(500em, 750em) / 100};
-      --top-offset: #{random_range(0vh, 10000vh) / 100};
-      --fall-duration: #{random_range(6000, 12000s) / 1000};
-      --fall-delay: #{random_range(0, 10000s) / 1000};
-    }
-  }
-
-  &::before,
-  &::after {
-    position: absolute;
-    content: "";
-    top: 0;
-    left: calc(var(--star-width) / -2);
-    width: var(--star-width);
-    height: 100%;
-    background: linear-gradient(45deg, transparent, currentColor, transparent);
-    border-radius: inherit;
-    animation: blink 2s linear infinite;
-  }
-
-  &::before {
-    transform: rotate(45deg);
-  }
-
-  &::after {
-    transform: rotate(-45deg);
-  }
-}
-
-@keyframes fall {
-  to {
-    transform: translate3d(-30em, 0, 0);
-  }
-}
-
-@keyframes tail-fade {
-  0%,
-  50% {
-    width: var(--star-tail-length);
-    opacity: 1;
-  }
-
-  70%,
-  80% {
-    width: 0;
-    opacity: 0.4;
-  }
-
-  100% {
-    width: 0;
-    opacity: 0;
-  }
-}
-
-@keyframes blink {
-  50% {
-    opacity: 0.6;
-  }
-}
-
 .authForm {
-  width: 355px;
-}
-
-@media (max-width: 400px) {
-  .authForm {
-    width: 300px;
-  }
+  width: 300px;
 }
 </style>
--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,72 +1,75 @@
 <template>
-  <q-page class="full-width row justify-center items-center">
-    <div class="bg-white card shadow-1 column q-pa-lg">
-      <!-- <div class="col-md-12 content-center"> -->
-      <q-form
-        @submit="submitSignUp"
-        @reset="reset"
-        ref="signupForm"
-        class="q-gutter-md authForm"
-      >
-        <div class="q-py-sm">
-          <div class="text-h5 text-primary">Create Your Account</div>
-          <div class="text-subtitle-2 text-secondary">
-            Enter your personal details to create your account
+  <q-page class="signUp">
+    <div class="full-width row justify-center items-start">
+      <div class="bg-white card shadow-1 column q-pa-lg">
+        <!-- <div class="col-md-12 content-center"> -->
+        <q-form
+          @submit="submitSignUp"
+          @reset="reset"
+          ref="signupForm"
+          class="q-gutter-md authForm"
+        >
+          <div class="q-py-sm text-center">
+            <div class="text-h5 text-primary">Create Your Account</div>
+            <div class="text-subtitle-2 text-secondary">
+              Enter your personal details to create your account
+            </div>
           </div>
-        </div>
 
-        <q-input
-          filled
-          v-model="userID"
-          label="Your user ID *"
-          hint="Your ASRI Identifier"
-          lazy-rules
-          dense
-          :rules="[
-            (val) => (val && val.length > 0) || 'Please enter you User ID',
-          ]"
-        />
-
-        <q-input
-          filled
-          dense
-          type="password"
-          v-model="password"
-          label="Your Password"
-          lazy-rules
-          :rules="[
-            (val) => (val !== null && val !== '') || 'Password is required',
-            (val) => val.length >= 6 || 'Password should at least 6 characters',
-          ]"
-        />
-
-        <q-toggle
-          v-model="acceptTerms"
-          label="I accept the license and terms"
-        />
-
-        <div>
-          <q-btn label="Submit" type="submit" color="primary" />
-          <q-btn
-            label="Reset"
-            type="reset"
-            color="primary"
-            flat
-            class="q-ml-sm"
+          <q-input
+            filled
+            v-model="userID"
+            label="Your user ID *"
+            hint="Your ASRI Identifier"
+            lazy-rules
+            dense
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please enter you User ID',
+            ]"
           />
-        </div>
 
-        <!-- Already have an account -->
-        <div class="text-center q-py-md">
-          <div class="">
-            Already have an account?
-            <router-link to="/" style="text-decoration: none"
-              >Sign In</router-link
-            >
+          <q-input
+            filled
+            dense
+            type="password"
+            v-model="password"
+            label="Your Password"
+            lazy-rules
+            :rules="[
+              (val) => (val !== null && val !== '') || 'Password is required',
+              (val) =>
+                val.length >= 6 || 'Password should at least 6 characters',
+            ]"
+          />
+
+          <q-toggle
+            v-model="acceptTerms"
+            label="I accept the license and terms"
+          />
+
+          <div>
+            <q-btn
+              label="CREATE ACCOUNT"
+              class="glossy"
+              type="submit"
+              size="md"
+              color="primary"
+              style="width: 100%"
+            />
           </div>
-        </div>
-      </q-form>
-      <!-- </div> -->
+        </q-form>
+        <!-- </div> -->
+      </div>
+    </div>
+
+    <!-- Already have an account -->
+    <div class="text-center q-py-md">
+      <div class="text-white">
+        Already have an account?
+        <router-link to="/" style="text-decoration: none" class="text-warning"
+          >Sign In</router-link
+        >
+      </div>
     </div>
   </q-page>
 </template>
